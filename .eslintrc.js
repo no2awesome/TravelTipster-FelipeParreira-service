ab7da91192ae5edaa04e0ede2f13423f9006c42a
Felipe Parreira <felipeparreirafb@gmail.com>
2018-12-19 07:40:03 -0800

    Add AirBnb linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 /**
- * These rules enforce the Hack Reactor Style Guide
+ * These rules enforce the AirBnB Style Guide
  *
  * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
+ *   https://github.com/airbnb/javascript
  */
 
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  "extends": "airbnb-base"
 };
