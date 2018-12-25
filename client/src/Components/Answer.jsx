63c2570f6911569aabfd8e8a3be8e10a84773cd0
Felipe Parreira <felipeparreirafb@gmail.com>
2018-12-25 10:54:45 -0800

    Style individual answers

--- client/src/Components/Answer.jsx
+++ client/src/Components/Answer.jsx
@@ -15,31 +15,78 @@ const Answer = (props) => {
     top: '35%',
   };
 
+  const answerStyle = {
+    fontSize: '14px',
+    lineHeight: '18px',
+    color: '#4a4a4a',
+    fontFamily: 'Arial',
+  };
+
+  const reviewedStyle = {
+    ...answerStyle,
+    color: '#000a12',
+  };
+
+  const flagStyle = {
+    fontSize: '14px',
+    color: '#b7b7b7',
+  };
+
+  const answerItemStyle = {
+    borderLeft: '1px solid #e5e5e5',
+    paddingLeft: '30px',
+    display: 'flex',
+    width: '700px',
+    justifyContent: 'space-between',
+    marginBottom: '30px',
+    position: 'relative',
+  };
+
+  const voteCountStyle = {
+    textAlign: 'center',
+    fontSize: '12px',
+    color: '#4a4a4a',
+    fontFamily: 'Arial',
+  };
+
+  const votingContainer = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100px',
+    float: 'right',
+  };
+
   return (
-    <li>
-      {answer.Content}
-      <br />
-      User: {user.Username}
-      <br />
-      Votes: {answer.Votes}
-      <br />
+    <li style={answerItemStyle}>
+      <div>
+        <p style={reviewedStyle}>Response from {user.Username} | Reviewed this property | <i className="fa fa-flag" style={flagStyle}></i> </p>
+        <p style={answerStyle}>{answer.Content}</p>
+      </div>
       {answer.UserID === props.currentUser.UserID
         ? <button className="btn-primary" onClick={props.deleteAnswer}>Delete</button>
         : null
       }
-      <button className="arrow"
-      onClick={() => props.voteAnswer(answer.UserID,
-        answer.QuestionID,
-        answer.id,
-        true)}>
-        <i className="fa fa-chevron-up" style={upArrow}></i></button>
-      <button className="arrow"
-      onClick={() => props.voteAnswer(answer.UserID,
-        answer.QuestionID,
-        answer.id,
-        false)}>
-          <i className="fa fa-chevron-down" style={downArrow}></i>
+      <div style={votingContainer}>
+        <button className="arrow"
+        onClick={() => props.voteAnswer(answer.UserID,
+          answer.QuestionID,
+          answer.id,
+          true)}>
+          <i className="fa fa-chevron-up" style={upArrow}></i></button>
+        <span style={voteCountStyle}>
+          {answer.Votes}
+          <br />
+          <span>Votes</span>
+        </span>
+        <button className="arrow"
+        onClick={() => props.voteAnswer(answer.UserID,
+          answer.QuestionID,
+          answer.id,
+          false)}>
+            <i className="fa fa-chevron-down" style={downArrow}></i>
         </button>
+      </div>
     </li>
   );
 };
--- client/src/Components/Question.jsx
+++ client/src/Components/Question.jsx
@@ -1,19 +1,35 @@
 import React from 'react'; // eslint-disable-line no-unused-vars,import/no-unresolved
+import moment from 'moment';
 import AnswerList from './AnswerList.jsx'; // eslint-disable-line no-unused-vars
 
 const Question = (props) => {
   const { question } = props;
+  const questionStyle = {
+    fontSize: '16px',
+    lineHeight: '22px',
+    color: '#000a12',
+    fontFamily: 'Arial',
+  };
+
+  const flagStyle = {
+    fontSize: '10px',
+    color: '#b7b7b7',
+  };
+
+  const dateStyle = {
+    fontSize: '12px',
+    color: '#4a4a4a',
+    fontFamily: 'Arial',
+  };
 
   return (
     <li>
-
       User: {question.User ? question.User.Username : null}
       <br />
       <img src={question.User ? question.User.ThumbnailURL : null} />
       <br />
-      {question.Content}
-      <br />
-      {question.PostedDate}
+      <p style={questionStyle}>{question.Content}</p>
+      <p style={dateStyle}>{moment(question.PostedDate).format('LL')} | <i className="fa fa-flag" style={flagStyle}></i></p>
       <br />
       {question.UserID === props.currentUser.UserID
         ? <button className="btn-primary small" onClick={props.deleteQuestion}>Delete</button>
