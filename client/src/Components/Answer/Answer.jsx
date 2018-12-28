ab41a27cb1a82947ed44518bf841b890381f5402
Felipe Parreira <felipeparreirafb@gmail.com>
2018-12-27 23:23:29 -0800

    Implement partially user stats for answers (styling is still an issue)

--- client/src/Components/Answer/Answer.jsx
+++ client/src/Components/Answer/Answer.jsx
@@ -1,46 +1,68 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+import UserStats from '../UserStats/UserStats.jsx'; // eslint-disable-line no-unused-vars
 import styles from './Answer.css';
 import genStyles from '../App/App.css';
 
-// import UserStats from '../UserStats/UserStats.jsx'; // eslint-disable-line no-unused-var
 // import CssModules from 'react-css-modules';
 
-const Answer = (props) => {
-  const { answer, user } = props;
+class Answer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showUserStats: false,
+    };
 
-  // <UserStats user={user} />
-  return (
-    <li className={`${styles.answerItemStyle} answer`}>
-      <div>
-        <p className={styles.reviewedStyle}>Response from {user.Username} | Reviewed this property | <i className={`${styles.flagStyle} fa fa-flag`}></i> </p>
-        <p className={styles.answerStyle}>{answer.Content}</p>
-        {answer.UserID === props.currentUser.UserID
-          ? <button className="btn-primary small" onClick={props.deleteAnswer}>Delete</button>
-          : null
-        }
-      </div>
-      <div className={styles.votingContainer}>
-        <button className={genStyles.arrow}
-        onClick={() => props.voteAnswer(answer.UserID,
-          answer.QuestionID,
-          answer.id,
-          true)}>
-          <i className={`${styles.upArrow} fa fa-chevron-up`}></i></button>
-        <span className={styles.voteCountStyle}>
-          {answer.Votes}
-          <br />
-          <span>Votes</span>
-        </span>
-        <button className={genStyles.arrow}
-        onClick={() => props.voteAnswer(answer.UserID,
-          answer.QuestionID,
-          answer.id,
-          false)}>
-            <i className={`${styles.downArrow} fa fa-chevron-down`}></i>
-        </button>
-      </div>
-    </li>
-  );
-};
+    this.toggleShowUserStats = this.toggleShowUserStats.bind(this);
+  }
+
+  toggleShowUserStats() {
+    this.setState({
+      showUserStats: !this.state.showUserStats,
+    });
+  }
+
+  render() {
+    const { answer, user } = this.props;
+
+    return (
+      <li className={`${styles.answerItemStyle} answer`}>
+        <div className={styles.answerSum} onMouseEnter={this.toggleShowUserStats}
+        onMouseLeave={this.toggleShowUserStats}>
+          <p className={styles.reviewedStyle}>Response from {user.Username} | Reviewed this property | <i className={`${styles.flagStyle} fa fa-flag`}></i> </p>
+          <p className={styles.answerStyle}>{answer.Content}</p>
+          {answer.UserID === this.props.currentUser.UserID
+            ? <button className={`${genStyles['btn-primary']} ${genStyles.small}`} onClick={this.props.deleteAnswer}>Delete</button>
+            : null
+          }
+          {this.state.showUserStats
+            ? <UserStats user={user} toggleShowUserStats={this.toggleShowUserStats} 
+            className={styles.userStatsStyle}/>
+            : null
+          }
+        </div>
+        <div className={styles.votingContainer}>
+          <button className={genStyles.arrow}
+          onClick={() => this.props.voteAnswer(answer.UserID,
+            answer.QuestionID,
+            answer.id,
+            true)}>
+            <i className={`${styles.upArrow} fa fa-chevron-up`}></i></button>
+          <span className={styles.voteCountStyle}>
+            {answer.Votes}
+            <br />
+            <span>Votes</span>
+          </span>
+          <button className={genStyles.arrow}
+          onClick={() => this.props.voteAnswer(answer.UserID,
+            answer.QuestionID,
+            answer.id,
+            false)}>
+              <i className={`${styles.downArrow} fa fa-chevron-down`}></i>
+          </button>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default Answer;
