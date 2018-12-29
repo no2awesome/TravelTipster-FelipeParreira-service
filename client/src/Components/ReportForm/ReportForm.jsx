a6edbfc3c584d8fd644356fca9f8a7e609d3ce78
Felipe Parreira <felipeparreirafb@gmail.com>
2018-12-29 09:40:54 -0800

    Style validation of input for form report

--- client/src/Components/ReportForm/ReportForm.jsx
+++ client/src/Components/ReportForm/ReportForm.jsx
@@ -1,31 +1,64 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import styles from './ReportForm.css';
 import genStyles from '../App/App.css';
 
-const ReportForm = (props) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div>Report a Problem</div>
-        <div><i className={`${styles.wdwIconStyle} fa fa-times`}></i></div>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.description}>
-          <h4>Why are you reporting this?
-          <br />
-          Add specific details about why you
-          feel this question or answer is inappropriate.&nbsp;
-          <span className={styles.italic}>(optional)</span></h4>
+class ReportForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reportContent: '',
+      isInvalidInput: false,
+    };
+
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+  }
+
+  handleTextAreaChange(e) {
+    const input = e.target.value;
+    const inputLength = input.length;
+
+    this.setState({
+      reportContent: input,
+      isInvalidInput: inputLength < 50 || inputLength > 500,
+    });
+  }
+
+  render() {
+    let footNoteStyles = `${styles.footNote}`;
+    let btnStyles = `${genStyles['btn-primary']} ${genStyles.big} ${styles.submit}`;
+    if (this.state.isInvalidInput) {
+      footNoteStyles += ` ${styles.invalidInput}`;
+      btnStyles += ` ${genStyles.disabled}`;
+    }
+    if (this.state.reportContent.length === 0 && !this.state.isInvalidInput) {
+      btnStyles += ` ${genStyles.disabled}`;
+    }
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div>Report a Problem</div>
+          <div><i className={`${styles.wdwIconStyle} fa fa-times`}></i></div>
         </div>
-        <textarea className={styles.textInput}></textarea>
-        <div className={styles.footNote}>50 characters minimum, 500 maximum</div>
+        <div className={styles.body}>
+          <div className={styles.description}>
+            <h4>Why are you reporting this?
+            <br />
+            Add specific details about why you
+            feel this question or answer is inappropriate.&nbsp;
+            <span className={styles.italic}>(optional)</span></h4>
+          </div>
+          <textarea value={this.state.reportContent}
+          onChange={this.handleTextAreaChange} className={styles.textInput}></textarea>
+          <div className={footNoteStyles}>50 characters minimum, 500 maximum</div>
+        </div>
+        <div className={styles.footer}>
+          <button
+          className={btnStyles}>Submit</button>
+        </div>
       </div>
-      <div className={styles.footer}>
-        <button
-        className={`${genStyles['btn-primary']} ${genStyles.big} ${styles.submit}`}>Submit</button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ReportForm;
